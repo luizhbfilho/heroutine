340d9bd5f97263bbc529beaeb058364a838a99fe
luizhbfilho <luizhbfilho@outlook.com>
2023-02-01 15:01:51 -0300

    Add new file for colors.xml and themes.xml

--- app/src/main/java/com/luizhbfilho/heroutine/core/model/MealDomain.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/core/model/MealDomain.kt
@@ -1,7 +1,13 @@
 package com.luizhbfilho.heroutine.core.model
 
+import com.google.type.DateTime
+import java.util.Calendar
+
 data class MealDomain(
     val name: String = "",
+    var description: String = "",
+    var isCompleted: Boolean = false,
+    var dueDateTime: Calendar,
     val foods: MutableList<FoodDomain>?
 ) {
 }
