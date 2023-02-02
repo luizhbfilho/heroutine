59d94e3ecd31cd9c035ffb49c26c6b2a3ace4acd
luizhbfilho <luizhbfilho@outlook.com>
2023-02-02 15:39:20 -0300

    Activity Sign In layout implementation

--- app/src/main/java/com/luizhbfilho/heroutine/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/ui/activity/SignInActivity.kt
@@ -1,6 +1,16 @@
 package com.luizhbfilho.heroutine.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    private fun firebaseLogin(){
+
+    }
 }
