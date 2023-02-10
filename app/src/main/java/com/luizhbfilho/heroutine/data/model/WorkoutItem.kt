ce4132bca661f47f6d95c132456a578400742700
luizhbfilho <luizhbfilho@outlook.com>
2023-02-10 14:19:18 -0300

    New implementation for workout_card

--- app/src/main/java/com/luizhbfilho/heroutine/data/model/WorkoutItem.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/data/model/WorkoutItem.kt
@@ -5,6 +5,7 @@ import com.google.firebase.Timestamp
 data class WorkoutItem(
     val id: String? = null,
     var name: String? = null,
+    var description: String? = null,
     var isCompleted: Boolean? = null,
     var dueDate: Timestamp? = null,
     var exercises: MutableList<ExerciseItem>? = null
