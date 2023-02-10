ae805a4b72b48391f26252598b2ef14bee926215
luizhbfilho <luizhbfilho@outlook.com>
2023-02-09 23:14:59 -0300

    Changed Material version to 1.7.0

--- app/src/main/java/com/luizhbfilho/heroutine/ui/activity/MainActivity.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.luizhbfilho.heroutine.ui.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -33,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_account
             )
         )
+
+        supportActionBar?.hide()
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
     }
