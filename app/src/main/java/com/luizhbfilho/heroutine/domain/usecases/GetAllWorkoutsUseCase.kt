e3b0d7049f36d736e61218a84b8be492c798c5c7
luizhbfilho <luizhbfilho@outlook.com>
2023-03-06 15:22:54 -0300

    Gradle version catalog implementation

--- app/src/main/java/com/luizhbfilho/heroutine/domain/usecases/GetAllWorkoutsUseCase.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/domain/usecases/GetAllWorkoutsUseCase.kt
@@ -1,4 +1,4 @@
-package com.luizhbfilho.heroutine.core.usecases
+package com.luizhbfilho.heroutine.domain.usecases
 
 import com.luizhbfilho.heroutine.core.model.WorkoutItem
 
--- app/src/main/java/com/luizhbfilho/heroutine/ui/feature/start/GetStartedFragment.kt
+++ app/src/main/java/com/luizhbfilho/heroutine/ui/feature/start/GetStartedFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.luizhbfilho.heroutine.R
 import com.luizhbfilho.heroutine.databinding.FragmentGetStartedBinding
@@ -28,10 +30,15 @@ class GetStartedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentGetStartedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
         binding.signUpButton.setOnClickListener {
             findNavController().navigate(R.id.action_getStartedFragment_to_sign_up_fragment)
         }
-        return binding.root
     }
 
     override fun onDestroy() {
